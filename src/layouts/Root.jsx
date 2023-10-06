9c03c85e793a0621239d593dd6ccb874076b05b0
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-10-06 21:44:01 +0600

    active routes create

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
