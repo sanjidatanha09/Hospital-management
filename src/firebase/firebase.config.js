6223ec0dd8cc97c41be12c0421ba3b431c8ca24d
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-10-06 20:48:55 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCXeFDRgJug8s02mX6nb2cnBVPYO4a0JYM",
+    authDomain: "hospital-management-f641c.firebaseapp.com",
+    projectId: "hospital-management-f641c",
+    storageBucket: "hospital-management-f641c.appspot.com",
+    messagingSenderId: "112522867697",
+    appId: "1:112522867697:web:99333a189654f97f0a8a50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
